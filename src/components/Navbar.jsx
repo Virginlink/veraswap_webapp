a3ef07e8a79d7af39037c71ed7cbaea385073cc6
SujithStellargoldcoin <somraajsujith@gmail.com>
2021-01-23 13:54:27 +0530

    make it better

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -282,11 +282,11 @@ export default class Navbar extends Component {
                                                 <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M2 3h6a4 4 0 0 1 4 4v14a3 3 0 0 0-3-3H2z"></path><path d="M22 3h-6a4 4 0 0 0-4 4v14a3 3 0 0 1 3-3h7z"></path></svg>
                                                 VRAP
                                             </a>
-                                            <a className="link" target="_blank" rel="noopener noreferrer" href="#">
+                                            <a className="link" target="_blank" rel="noopener noreferrer" href="https://twitter.com/veraswap">
                                                 <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"></path></svg>
                                                 Twitter
                                             </a>
-                                            <a className="link" target="_blank" rel="noopener noreferrer" href="#">
+                                            <a className="link" target="_blank" rel="noopener noreferrer" href="https://t.me/veraswap">
                                                 <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M21 11.5a8.38 8.38 0 0 1-.9 3.8 8.5 8.5 0 0 1-7.6 4.7 8.38 8.38 0 0 1-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 0 1-.9-3.8 8.5 8.5 0 0 1 4.7-7.6 8.38 8.38 0 0 1 3.8-.9h.5a8.48 8.48 0 0 1 8 8v.5z"></path></svg>
                                                 Telegram
                                             </a>
