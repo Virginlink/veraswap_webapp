1b9596915ee1991bbd85f624077cdd6d03bc926d
SujithStellargoldcoin <somraajsujith@gmail.com>
2021-03-04 00:58:43 +0530

    adding LP Pools

--- src/components/Stake/Stake.jsx
+++ src/components/Stake/Stake.jsx
@@ -2,6 +2,12 @@ import React, { Component } from 'react'
 import VRAP from '../../assets/images/logo.png';
 import {withRouter} from 'react-router-dom';
 import AssetCard from './components/assetCard';
+import BNB from '../../assets/images/eth.png';
+import SWINGBY from '../../assets/images/empty-token.webp';
+import LIT from '../../assets/images/lit.webp';
+import SFP from '../../assets/images/sfp.webp';
+import DAI from '../../assets/images/dai.webp';
+
 const TWT = 'https://github.com/trustwallet/assets/blob/master/blockchains/binance/assets/TWT-8C2/logo.png?raw=true';
 const CAKE = 'https://github.com/trustwallet/assets/blob/master/blockchains/smartchain/assets/0x0E09FaBB73Bd3Ade0a17ECC321fD13a19e81cE82/logo.png?raw=true';
 const JULD = 'https://github.com/trustwallet/assets/blob/master/blockchains/smartchain/assets/0x5A41F637C3f7553dBa6dDC2D3cA92641096577ea/logo.png?raw=true';
@@ -11,6 +17,7 @@ const BURGER = 'https://github.com/trustwallet/assets/blob/master/blockchains/sm
 const BUSD = 'https://github.com/trustwallet/assets/blob/master/blockchains/smartchain/assets/0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56/logo.png?raw=true';
 const BUSDT = 'https://github.com/trustwallet/assets/blob/master/blockchains/smartchain/assets/0x55d398326f99059fF775485246999027B3197955/logo.png?raw=true';
 const XVS = 'https://github.com/trustwallet/assets/blob/master/blockchains/smartchain/assets/0xcF6BB5389c92Bdda8a3747Ddb454cB7a64626C63/logo.png?raw=true';
+const DODO = 'https://github.com/trustwallet/assets/blob/master/blockchains/smartchain/assets/0x67ee3Cb086F8a16f34beE3ca72FAD36F7Db929e2/logo.png?raw=true';
 
 class Stake extends Component {
     constructor(props) {
@@ -76,12 +83,48 @@ class Stake extends Component {
                     tokenContract: '0xcF6BB5389c92Bdda8a3747Ddb454cB7a64626C63',
                     background: 'radial-gradient(91.85% 100% at 1.84% 0%, rgb(252, 7, 125) 0%, rgb(108, 114, 132) 100%)',
                 },
-                // {
-                //     icons : [BUSDT,XVS],
-                //     ticker : 'CAKE_LP',
-                //     tokenContract : '0x610e7a287c27dfFcaC0F0a94f547Cc1B770cF483',
-                //     background: 'radial-gradient(91.85% 100% at 1.84% 0%, rgb(124, 118, 136) 0%, rgb(108, 114, 132) 100%)'
-                // }
+                {
+                    icons : [TWT,BNB],
+                    ticker : 'TWT-BNB',
+                    tokenContract : '0x610e7a287c27dfFcaC0F0a94f547Cc1B770cF483',
+                    background: 'radial-gradient(91.85% 100% at 1.84% 0%, rgb(124, 118, 136) 0%, rgb(108, 114, 132) 100%)'
+                },
+                {
+                    icons : [CAKE,BNB],
+                    ticker : 'CAKE-BNB',
+                    tokenContract : '0xA527a61703D82139F8a06Bc30097cC9CAA2df5A6',
+                    background: 'radial-gradient(91.85% 100% at 1.84% 0%, rgb(124, 118, 136) 0%, rgb(108, 114, 132) 100%)'
+                },
+                {
+                    icons : [DODO,BNB],
+                    ticker : 'DODO-BNB',
+                    tokenContract : '0x9e642d174B14fAEa31D842Dc83037c42b53236E6',
+                    background: 'radial-gradient(91.85% 100% at 1.84% 0%, rgb(124, 118, 136) 0%, rgb(108, 114, 132) 100%)'
+                },
+                {
+                    icons : [SWINGBY,BNB],
+                    ticker : 'SWINGBY-BNB',
+                    tokenContract : '0x4576C456AF93a37a096235e5d83f812AC9aeD027',
+                    background: 'radial-gradient(91.85% 100% at 1.84% 0%, rgb(124, 118, 136) 0%, rgb(108, 114, 132) 100%)'
+                },
+                {
+                    icons : [SFP,BNB],
+                    ticker : 'SFP-BNB',
+                    tokenContract : '0xcBe2cF3bd012e9C1ADE2Ee4d41DB3DaC763e78F3',
+                    background: 'radial-gradient(91.85% 100% at 1.84% 0%, rgb(124, 118, 136) 0%, rgb(108, 114, 132) 100%)'
+                },
+                {
+                    icons : [LIT,BNB],
+                    ticker : 'LIT-BNB',
+                    tokenContract : '0x60bB03D1010b99CEAdD0dd209b64bC8bd83da161',
+                    background: 'radial-gradient(91.85% 100% at 1.84% 0%, rgb(124, 118, 136) 0%, rgb(108, 114, 132) 100%)'
+                },
+                {
+                    icons : [DAI,BNB],
+                    ticker : 'DAI-BNB',
+                    tokenContract : '0x3aB77e40340AB084c3e23Be8e5A6f7afed9D41DC',
+                    background: 'radial-gradient(91.85% 100% at 1.84% 0%, rgb(124, 118, 136) 0%, rgb(108, 114, 132) 100%)'
+                },
             ]
         }
     }
