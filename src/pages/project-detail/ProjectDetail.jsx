402493933dbee483d22120a64233eb185b4d2758
Saran RTS <rtssaran@gmail.com>
2021-08-23 08:42:38 +0530

    hotfix - removed console logs

--- src/pages/project-detail/ProjectDetail.jsx
+++ src/pages/project-detail/ProjectDetail.jsx
@@ -64,7 +64,7 @@ class ProjectDetail extends Component {
 				fetchPolicy: "network-only",
 			})
 			.then(async (res) => {
-				console.log(res.data.project);
+				// console.log(res.data.project);
 				if (res.data.project) {
 					const ownerWallets = [
 						res.data.project.owner.toLowerCase(),
